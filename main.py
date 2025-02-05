0378c198346922c56d7c012af9e4afe445cec09b
superstan <stancuseac@gmail.com>
2025-02-04 18:36:25 -0800

    added player class and drew him to the screen

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame, constants
+import pygame, constants, player
 from constants import *
+from player import*
 
 def main ():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
@@ -15,7 +18,14 @@ def main ():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
+        
+        x = SCREEN_WIDTH / 2
+        y = SCREEN_HEIGHT / 2
+        player = Player(x, y)
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 # convert milliseconds to seconds
 
 if __name__ == "__main__":
     main()
