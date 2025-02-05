af09465e267991d7bfc136cd3420c0471d665216
superstan <stancuseac@gmail.com>
2025-02-04 17:37:52 -0800

    initial game loop and gitignore file added

--- main.py
+++ main.py
@@ -2,11 +2,20 @@
 # the open-source pygame library
 # throughout this file
 import pygame, constants
+from constants import *
 
 def main ():
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,(0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
