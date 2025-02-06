e44840a6a56993db0a7bb1c6f5880c437ddd622f
superstan <stancuseac@gmail.com>
2025-02-05 20:00:14 -0800

    add player rotation and update of sprite

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame, constants, player
+import pygame
 from constants import *
-from player import*
+from player import Player
 
 def main ():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
     print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
-        
-        x = SCREEN_WIDTH / 2
-        y = SCREEN_HEIGHT / 2
-        player = Player(x, y)
+            
+        player.update(dt)
+        screen.fill("black")
         player.draw(screen)
-
         pygame.display.flip()
-        dt = clock.tick(60) / 1000 # convert milliseconds to seconds
+
+        dt = clock.tick(60) / 1000 # convert milliseconds to seconds and limit framerate to 60 FPS
 
 if __name__ == "__main__":
     main()
